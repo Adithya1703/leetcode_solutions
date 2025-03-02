83f3a287f5460cc96d2c306fcfd6939c4995c31e
Adithya Sharma Mukurala <adithyasharma1703@gmail.com>
2025-03-02 15:10:25 +0530

    optimized cpp solution for add string binary by removing usage of reverse

--- Difficult_Easy/Add_string_binary.cpp
+++ Difficult_Easy/Add_string_binary.cpp
@@ -20,7 +20,7 @@ a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 */
 
-//cpp - brute force, time complexity - O(N)
+//cpp - time complexity - O(N)
 #include<string>
 #include<algorithm>
 using namespace std;
@@ -28,19 +28,17 @@ using namespace std;
 class Solution {
   public:
       string addBinary(string a, string b) {
-          int i = a.length()-1;
-          int j = b.length()-1;
-          string output = "";
-          int carry = 0;
-          while(i>=0 || j>=0 || carry){
-              int sum = carry;
-              if(i>=0) sum+=a[i--]-'0';
-              if(j>=0) sum+=b[j--]-'0';
-              int bin = sum%2;
-              carry = sum/2;
-              output+=(bin+'0');
-          }
-          reverse(output.begin(), output.end());
-          return output;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        string output = "";
+        int carry = 0;
+        while(i>=0||j>=0||carry){
+          int sum = carry;
+          if(i>=0) sum+=a[i--]+'0';
+          if(j>=0) sum+=b[j--]+'0';
+          carry = sum/2;
+          output.insert(output.begin(),(sum%2)+'0');
+        }
+        return output;
       }
   };
